6d6262c9c0d4a66f5572c66b2c6e50ca1cb7f4c1
Dylan Cochrane <dylan.cochrane@capitalone.com>
2019-08-01 16:00:51 -0400

    removed extra test data and narrowed test criteria

--- tests/test_rds.py
+++ tests/test_rds.py
@@ -1261,19 +1261,12 @@ class TestModifyVpcSecurityGroupsAction(BaseTest):
         # Uses the add_security_group data--should match 2 DB instances
         # Checks that the expected VPC is present
 
-        session_factory = self.replay_flight_data("test_rds_filter_by_vpcid")
+        session_factory = self.replay_flight_data("test_rds_add_security_group")
         p = self.load_policy(
             {
                 "name": "filter-by-vpcid",
                 "resource": "rds",
                 "filters": [
-                    {"type": "security-group", "key": "GroupName", "value": "default"},
-                    {
-                        "type": "value",
-                        "key": "DBInstanceIdentifier",
-                        "value": "test-sg-fail.*",
-                        "op": "regex",
-                    },
                     {
                         "type": "vpc",
                         "key": "VpcId",
@@ -1288,7 +1281,7 @@ class TestModifyVpcSecurityGroupsAction(BaseTest):
 
         resources = p.run()
 
-        self.assertEqual(len(resources), 2)
+        self.assertEqual(len(resources), 4)
         self.assertEqual("vpc-09b75e60", resources[0]["DBSubnetGroup"]["VpcId"])
 
 
